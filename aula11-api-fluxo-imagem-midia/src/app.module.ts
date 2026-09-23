6390cbd9f93c858616f85e84e3d53303aaede345
Enzo Capitani <enzo.l.vieira@aluno.senai.br>
2026-09-23 17:02:26 -0300

    feat: carregamento de arquivos de imagens especificos via requisição http

---
+++ aula11-api-fluxo-imagem-midia/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { MidiaController } from './midia.controller.js';
+import { MidiaModule } from './midia.module.js';
+
+@Module({
+  imports: [
+  ],
+  controllers: [AppController, MidiaController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ aula11-api-fluxo-imagem-midia/src/midia.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { MidiaController } from "./midia.controller.js";
+
+@Module({
+    controllers:[MidiaController]
+})
+
+export class MidiaModule{
+
+}
